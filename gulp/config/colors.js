c96e0d9eaa9e192369694ef7c41e354a408913a8
Nati Kaumusher <natan.kamuhser@gmail.com>
2018-12-15 22:57:10 +0200

    Completed the large hero section

--- gulp/config/colors.js
+++ gulp/config/colors.js
@@ -1,5 +1,10 @@
 module.exports = {
 	mainBlue: '#2a4b6a',
 	mainOrange: '#cf8836',
-	darkBlue: "#01274b"
+	mainGreen: '#55c57a',
+	mainWhite: '#ffffff',
+	darkBlue: '#01274b',
+	lightGreen: '#7ed56f',
+	darkGreen: '#28b485',
+	lightBlack: '#b79d9d'
 }
